cd42e43eaec4e559147f2c8b9862c24aba418fa8
Amitesh Mishra <mishraamitesh738@gmail.com>
2024-11-19 18:06:39 +0530

    Completed Currency Project

---
+++ currency Convertor (custom react)/src/components/index.js
@@ -0,0 +1,4 @@
+//  so this file we genrally create in bog code bases here we import every components and then we will export each components from here
+import InputBox from "./InputBox";
+
+export {InputBox}
